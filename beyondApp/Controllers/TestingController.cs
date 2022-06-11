84660a53bcf5c67b7b3b1bddbf2a7508bfe2d36a
TD\as255186 <abhilash.sharma@teradata.com>
2022-06-11 04:57:31 -0700

    main project created

---
+++ beyondApp/Controllers/TestingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beyondApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        private readonly ILogger<TestingController> _logger;
+
+        public TestingController(ILogger<TestingController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ beyondApp/Helpers/ReformatValidation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beyondApp.Helpers
+{
+    public class ReformatValidation : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Result is BadRequestObjectResult badRequestObjectResult)
+            {
+                if (badRequestObjectResult.Value is ValidationProblemDetails)
+                {
+                    context.Result = new BadRequestObjectResult(GenericBadRequest());
+                    //context.Result.Value.Errors = null;
+                }
+            }
+
+
+            base.OnResultExecuting(context);
+        }
+
+        public static ValidationProblemDetails GenericBadRequest()
+        {
+            var problemDetails = new ValidationProblemDetails()
+            {
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                Title = "Bad Request",
+                Status = StatusCodes.Status400BadRequest
+            };
+            return problemDetails;
+        }
+    }
+}
+}
